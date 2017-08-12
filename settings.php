99442e1416613a739a69214082e66b35451b69f6
Joanne Connor <lullabelle@users.noreply.github.com>
2017-08-12 16:01:25 +0100

    Update settings.php

--- settings.php
+++ settings.php
@@ -2,9 +2,9 @@
 session_start();
 
 return [
-    'client_id' => 'xxx', //this id given by instagram when registering client
-    'client_secret' => 'xxx',// ""
-    'redirect_uri' => 'http://' . $_SERVER['HTTP_HOST'] . '/Practicals/Practical5/complete-oauth.php',
+    'client_id' => 'bd12f559648b44b19a9fb34e63b8b752', //this id given by instagram when registering client
+    'client_secret' => '52de3f7af49444e6a715678346f0d892',// ""
+    'redirect_uri' => 'http://' . $_SERVER['HTTP_HOST'] . '/complete-oauth.php',
     'scopes' => [
         'likes',
         'basic',
